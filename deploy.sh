367f14983db85a324cd17756235f59f02f4ec994
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-12-05 09:59:54 +0800

    add: Create deployment configuration files

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort errors
+set -e
+
+# build
+npm run build
+
+cp ./404.html dist
+
+cd dist
+
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:LuizPelegrini/coffee-delivery.git main:gh-pages
+
+cd -
