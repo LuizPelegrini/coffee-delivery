56ee19d7c0fadd92da44f3aee9bc92e867186c7d
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-12-05 11:57:17 +0800

    update: Trying Vite renderBuiltUrl function

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/coffee-delivery/',
+  experimental: {
+    renderBuiltUrl(filename, details) {
+      if (details.type === 'public') {
+        return `https://luizpelegrini.github.io/coffee-delivery/${filename}`;
+      }
+    },
+  },
 });
